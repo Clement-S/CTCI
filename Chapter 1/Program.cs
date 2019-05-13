98a955bde4f76e1c4135c7493a3c7ae9e7eb3614
Seyi <coadedeji@gmail.com>
2019-05-13 21:09:37 +0100

    q1.1 solved

---
+++ Chapter 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Q1_1();
+            var result = test.isUnique("clement");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter 1/Q1_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_1
+{
+    class Q1_1
+    {
+       public bool isUnique(string input)
+        {
+            var characters = input.ToCharArray();         
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                for (int j = i + 1; j < characters.Length; j++)
+                    if (characters[i] == characters[j])
+                    {
+                        return false;
+                    }
+            }
+            return true;
+        }
+    }
+}
